d3129b257d150eef2eba0e38e42775f06cde0490
BagrijRoman <bagrijroman@gmail.com>
2017-03-08 12:16:04 +0200

    initial setup

---
+++ README.md
@@ -0,0 +1,8 @@
+A [Metalsmith](http://www.metalsmith.io) project.
+
+Run npm install to install packages
+
+For building project run `npm run build`
+Project should appear in build folder.
+
+Have a nice day)
---
+++ build.js
@@ -0,0 +1,48 @@
+
+var Metalsmith   = require('./node_modules/metalsmith'),
+    drafts       = require('./node_modules/metalsmith-drafts'),
+    helpers      = require('metalsmith-register-helpers'),
+    collections  = require('metalsmith-collections'),
+    markdown     = require('./node_modules/metalsmith-markdown'),
+    layouts      = require('./node_modules/metalsmith-layouts'),
+    cleanCSS     = require('metalsmith-clean-css'),
+    uglify       = require('metalsmith-uglify'),
+    htmlMinifier = require("metalsmith-html-minifier");
+
+
+//  less         = require('./node_modules/metalsmith-less'),
+//  sitemap      = require('metalsmith-mapsite'),
+
+
+
+Metalsmith(__dirname)
+  .source('./src')
+  .destination('./build')
+  .clean(true)
+  // .metadata({})
+  .use(drafts())
+  .use(helpers({
+    directory: 'template-helpers',
+  }))
+  .use(collections({
+
+  }))
+  .use(markdown())
+
+  .use(layouts({
+    engine: 'handlebars',
+    partials: 'partials'
+  }))
+  .use(cleanCSS({
+    files: 'imports/css/**',
+  }))                            // css minificator
+  .use(uglify())                 // js minificator
+  .use(htmlMinifier("*.html", {  // html minificator
+    removeComments: true,
+    removeEmptyAttributes: true,
+  }))
+  .build(function(err) {
+  if (err)
+    console.log(err);
+    //throw err;
+  });
