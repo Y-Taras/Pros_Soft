5a0506893ecfb3edbd19698a97018d33cfe2d474
BagrijRoman <bagrijroman@gmail.com>
2017-03-14 13:31:42 +0200

    added slider

--- src/imports/js/landing.js
+++ src/imports/js/landing.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
     auto: true
   });
 
-  $('.bx-wrapper').css('border', 'none');
-  $('#landing-slider').css('margin-top', '0');
-
-  $( "#landing-slider li" ).each(function( index ) {
-    this.style.width = parseInt(this.style.width) + 10 + 'px';
-  });
+  // $('.bx-wrapper').css('border', 'none');
+  // $('#landing-slider').css('margin-top', '0');
+  //
+  // $( "#landing-slider li" ).each(function( index ) {
+  //   this.style.width = parseInt(this.style.width) + 10 + 'px';
+  // });
 
 });
