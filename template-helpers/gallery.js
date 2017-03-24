577bc4137566dd449743e4a10f3dc2f3a06e58e9
ruslanV <ruslan.vasylenko87@gmail.com>
2017-03-24 16:45:21 +0200

    gallary helper

--- template-helpers/gallery.js
+++ template-helpers/gallery.js
@@ -1,11 +1,13 @@
 const gallery = (...srcs) => {
   srcs.pop();
+  const start = '<div class="app-gallery"><div class="row">';
   const col = '<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6">';
   const wrap = '<div class="img-wrapper">';
-  const gallery = srcs.reduce(
-    (prev, curr) => `${prev}${col}${wrap}<img class="app-gallery-item" src="${curr}"></div></div>`,
-    '<div class="app-gallery"><div class="row">');
-  return `${gallery}</div></div>`;
+  const end = '</div></div>';
+  const content = srcs.reduce((prev, curr) => (
+    `${prev}${col}${wrap}<a class="fancybox" href="${curr}" rel="gallery"><img class="app-gallery-item" src="${curr}"></a>${end}`
+  ), start);
+  return `${content}${end}`;
 };
 
 module.exports = gallery;
