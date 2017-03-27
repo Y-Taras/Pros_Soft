ed0a2c4e7cc9d02ccf448b99d88a5d75e749e3bf
ruslanV <ruslan.vasylenko87@gmail.com>
2017-03-27 17:18:38 +0300

    doors section added

--- template-helpers/gallery.js
+++ template-helpers/gallery.js
@@ -1,8 +1,8 @@
 const _ = require('lodash');
 
-const gallery = (path, nImg) => {
+const gallery = (colClass, path, nImg) => {
   const start = '<div class="app-gallery"><div class="row">';
-  const col = '<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6">';
+  const col = `<div class="${colClass || 'col-lg-2 col-md-2 col-sm-4 col-xs-6'}">`;
   const wrap = '<div class="img-wrapper">';
   const end = '</div></div>';
   const srcs = new _.fill(new Array(nImg), path).map((e, i) => `${e}${i < 9 ? '0' : ''}${i + 1}.jpg`);
