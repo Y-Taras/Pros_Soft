333978640c8d2f342e684ffbc2ee5bb86f0b66bc
ruslanV <ruslan.vasylenko87@gmail.com>
2017-03-27 15:40:00 +0300

    garden-houses section added

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 A [Metalsmith](http://www.metalsmith.io) project.
 
-Run npm install to install packages
+Run `npm install` to install packages
 
 For building project run `npm run build`
 Project should appear in build folder.
+
+To mount the app on a local machine on `<port>`, run in the directory `/build`:
+`sudo python3 -m http.server <port>`
 
 Have a nice day)
--- template-helpers/gallery.js
+++ template-helpers/gallery.js
@@ -1,11 +1,13 @@
-const gallery = (...srcs) => {
-  srcs.pop();
+const _ = require('lodash');
+
+const gallery = (path, nImg) => {
   const start = '<div class="app-gallery"><div class="row">';
   const col = '<div class="col-lg-2 col-md-2 col-sm-4 col-xs-6">';
   const wrap = '<div class="img-wrapper">';
   const end = '</div></div>';
+  const srcs = new _.fill(new Array(nImg), path).map((e, i) => `${e}${i < 9 ? '0' : ''}${i + 1}.jpg`);
   const content = srcs.reduce((prev, curr) => (
-    `${prev}${col}${wrap}<a class="fancybox" href="${curr}" rel="${srcs[0]}"><img class="app-gallery-item" src="${curr}"></a>${end}`
+    `${prev}${col}${wrap}<a class="fancybox" href="${curr}" rel="${path}"><img class="app-gallery-item" src="${curr}"></a>${end}`
   ), start);
   return `${content}${end}`;
 };
