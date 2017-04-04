1228aaec19d317f12688089a1927b82fd8db1fc8
ruslanV <ruslan.vasylenko87@gmail.com>
2017-04-04 13:47:23 +0300

    arches section. Responsive imgs in all app. Fixed bugs.

--- template-helpers/gallery.js
+++ template-helpers/gallery.js
@@ -1,9 +1,9 @@
 const _ = require('lodash');
 
-const gallery = (colClass, path, nImg) => {
+const gallery = (colClass, wrapClass, path, nImg) => {
   const start = '<div class="app-gallery"><div class="row">';
   const col = `<div class="${colClass || 'col-lg-2 col-md-2 col-sm-4 col-xs-6'}">`;
-  const wrap = '<div class="img-wrapper">';
+  const wrap = `<div class="img-wrapper ${wrapClass}">`;
   const end = '</div></div>';
   const srcs = new _.fill(new Array(nImg), path).map((e, i) => `${e}${i < 9 ? '0' : ''}${i + 1}.jpg`);
   const content = srcs.reduce((prev, curr) => (
