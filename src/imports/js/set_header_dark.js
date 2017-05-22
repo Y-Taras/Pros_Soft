dae031344dec0f4330f0c460cfa4f78f5d4ba50e
ruslanV <ruslan.vasylenko87@gmail.com>
2017-05-22 21:07:56 +0300

    Rewards gallary; menu subitems and sidebar restyled. All responsive

--- src/imports/js/set_header_dark.js
+++ src/imports/js/set_header_dark.js
@@ -3,6 +3,10 @@ $(document).scroll(() => {
   if (height) {
     if ($('header').offset().top > height - 0.2 * height) {
       $('header').removeClass('transparent');
-    } else $('header').addClass('transparent');
+      $('header .nav-item-block.transparent').removeClass('transparent');
+    } else {
+      $('header').addClass('transparent');
+      $('header .nav-item-block.transparent').addClass('transparent');
+    }
   }
 });
--- template-helpers/gallery.js
+++ template-helpers/gallery.js
@@ -1,29 +1,17 @@
 const _ = require('lodash');
 
+const getPicRefs = (n, path) => new _.fill(new Array(n), path).map((e, i) => `${e}${i < 9 ? '0' : ''}${i + 1}.png`);
+
 const gallery = (colClass, wrapClass, path, nImg) => {
   const start = '<div class="app-gallery"><div class="row">';
   const col = `<div class="${colClass || 'col-lg-2 col-md-2 col-sm-4 col-xs-6'}">`;
   const wrap = `<div class="img-wrapper ${wrapClass}">`;
   const end = '</div></div>';
-  const srcs = new _.fill(new Array(nImg), path).map((e, i) => `${e}${i < 9 ? '0' : ''}${i + 1}.jpg`);
+  const srcs = getPicRefs(nImg, path);
   const content = srcs.reduce((prev, curr) => (
     `${prev}${col}${wrap}<a class="fancybox" href="${curr}" rel="${path}"><img class="app-gallery-item" src="${curr}"></a>${end}`
   ), start);
   return `${content}${end}`;
-};
-
-const getPicRefs = (n, path) => new _.fill(new Array(n), path).map((e, i) => `${e}${i < 9 ? '0' : ''}${i + 1}.png`);
-
-const landingGallery = (thumbnailsPath, originsPath, nImg) => {
-  const thumbsSrcs = getPicRefs(nImg, thumbnailsPath);
-  const originsRefs = getPicRefs(nImg, originsPath);
-  const rows = _.chunk(thumbsSrcs, 2);
-  return rows.map((row, i) => {
-    const cols = row.map((col, j) => {
-      return `<div class="col-xs-6"><a class="fancybox" href="${originsRefs[2 * i + j]}"><img class="gallery-item" src="${col}"/></a></div>`;
-    }).join('');
-    return `<div class="row" style="margin-bottom: 10px">${cols}</div>`;
-  }).join('');
 };
 
 const noColGallery = (thumbnailsPath, originsPath, nImg) => {
@@ -34,4 +22,14 @@ const noColGallery = (thumbnailsPath, originsPath, nImg) => {
   }).join('');
 };
 
-module.exports = { gallery, landingGallery, noColGallery };
+const hiddenGallery = (path, nImg) => {
+  const refs = getPicRefs(nImg, path);
+  const mainImg = `<a class="fancybox" href="${refs[0]}" rel="${path}"><img class="gallery-img" src="${path}gallery_img1.png"/></a>`;
+  refs.shift();
+  const imgs = refs.map(ref => {
+    return `<a class="fancybox" href="${ref}" rel="${path}"></a>`
+  }).join('');
+  return `${mainImg}${imgs}`;
+};
+
+module.exports = { gallery, noColGallery, hiddenGallery };
