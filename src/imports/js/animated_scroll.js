28416257b8407851c9e871daca14fe0b4e26884e
ruslanV <ruslan.vasylenko87@gmail.com>
2017-07-21 12:17:20 +0300

    awards swippers  width changed

--- src/imports/js/animated_scroll.js
+++ src/imports/js/animated_scroll.js
@@ -4,9 +4,4 @@ $().ready(() => {
       scrollTop: $($(".block-container")[0]).offset().top - 65
     }, 1000);
   });
-  // $('.footer-logo-text').click(() => {
-  //   $('html, body').animate({
-  //     scrollTop: 0
-  //   }, 1000);
-  // });
 });
